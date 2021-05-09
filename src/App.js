89e1248cf24dd75e049a4c27923dea4ef06d8641
Rao Fahad <raofahad046@gmail.com>
2021-05-08 21:54:52 -0700

    finished the counter application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
 	return (
 		<div className="App">
 			<h1>Counter App With Redux-Toolkit</h1>
+			<Counter />
 		</div>
 	);
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, reset } from "../store/counterSlice";
 
 const Counter = () => {
-	return <div></div>;
+	const dispatch = useDispatch();
+	const count = useSelector((state) => {
+		return state.counter.count;
+	});
+	return (
+		<div>
+			<h3>Count: {count}</h3>
+			<div>
+				<button
+					onClick={() => {
+						dispatch(increment());
+					}}
+				>
+					Increment
+				</button>
+				<button
+					onClick={() => {
+						dispatch(decrement());
+					}}
+				>
+					Decrement
+				</button>
+				<button
+					onClick={() => {
+						dispatch(reset());
+					}}
+				>
+					Reset
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default Counter;
